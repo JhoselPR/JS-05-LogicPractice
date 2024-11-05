fed5612d8a3a1b465d8c0b472042e72ae187c99a
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2024-11-04 19:03:55 -0600

    feat: palindrome works with accented vowels

--- palindrome.js
+++ palindrome.js
@@ -6,7 +6,8 @@ Write a program that prompts for a word or sentence (it can be capitalized, have
 const sentence = prompt('Ingresa una palabra o frase para verificar si palíndromo');
 
 const isPalindrome = (input) => {
-    const newInput = input.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()¿?¡! ]/g,"").replace(/\s{2,}/g," ");
+    const newInput = input.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()¿?¡! ]/g,"").replace(/\s{2,}/g," ").replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u');
+    console.log(newInput);
     const reversedInput = newInput.split('').reverse().join('');
     if (reversedInput==newInput){
         console.log('Es palíndromo');
